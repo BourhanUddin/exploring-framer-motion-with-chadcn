afbf1abe300985002139b4f7d7a549f40dbed8c8
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2024-03-01 01:00:39 +0600

    animated navbar and fixed down and side scrollbar problems

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,10 +1,28 @@
+import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
-
 const Navbar = () => {
+  const NabAnimated = {
+    hidden: {
+      y: -60,
+    },
+    visible: {
+      y: 0,
+      transition: {
+        delay: 0.7,
+        type: "spring",
+        stiffness: 120,
+      },
+    },
+  };
   return (
     <header className=" h-16  ">
-      <nav className="w-full h-full flex justify-between items-center px-[20px] max-w-[1220px]  mx-auto">
+      <motion.nav
+        className="w-full h-full flex justify-between items-center px-[20px] max-w-[1220px]  mx-auto"
+        variants={NabAnimated}
+        initial="hidden"
+        animate="visible"
+      >
         <span className="text-3xl">Fixer</span>
         <ul className=" space-x-4">
           <NavLink to={"/"}>Home</NavLink>
@@ -13,7 +31,7 @@ const Navbar = () => {
           <NavLink to={"/about"}>About Us</NavLink>
           <Button>login</Button>
         </ul>
-      </nav>
+      </motion.nav>
     </header>
   );
 };
